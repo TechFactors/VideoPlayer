66fd3608ff7d46d6b6ce846e51168977b11ef7cf
florenmoya <42061578+florenmoya@users.noreply.github.com>
2020-03-11 14:21:09 +0800

    Initial commit

---
+++ Assets/VideoPlane.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoPlane : MonoBehaviour
+{
+    public VideoPlayer video;
+    public GameObject VideoPlayCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        video.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+        VideoPlayCanvas.SetActive(true);
+    }
+}
